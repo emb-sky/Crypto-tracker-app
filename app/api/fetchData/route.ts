9b545dad4be8a3ffd2142d2fee26faaf204e4d4b
Akash <emb-akasmah@embitel.com>
2024-07-31 12:02:43 +0530

    its second push

--- app/api/fetchData/route.ts
+++ app/api/fetchData/route.ts
@@ -3,14 +3,17 @@ import axios from 'axios'
 import clientPromise from '@/lib/mongodb'
 
 export async function GET() {
+  console.log('Starting GET request');
   try {
     const client = await clientPromise
     const db = client.db('Stock')
     const collection = db.collection('priceData')
-
+    console.log('Starting GET request');
     const symbols = ['bitcoin', 'ethereum', 'dogecoin', 'cardano', 'polkadot']
     const response = await axios.get(`${process.env.STOCK_API_URL}?ids=${symbols.join(',')}&vs_currencies=usd&x_cg_demo_api_key=${process.env.CG_API_KEY}`)
     const data = response.data
+
+    console.log('Fetched data:', data);
 
     const timestamp = new Date()
     for (const symbol of symbols) {
--- app/api/getPriceData/route.ts
+++ app/api/getPriceData/route.ts
@@ -5,6 +5,8 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const symbol = searchParams.get('symbol')
 
+  console.log('Symbol:', symbol);
+
   if (!symbol) {
     return NextResponse.json({ error: 'Symbol is required' }, { status: 400 })
   }
